40be7d45652f22a6b2dbdecaadcce314fd32a3d0
EklavyaPrasad <eklavyaprasad1@gmail.com>
2023-06-30 13:44:59 +0530

    Js objects practice done

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,28 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const car={
+    name: "Tarzan",
+    noOfWheels: 4,
+    type: "sports",
+    headlight: false,
+    color: "blue",
+    fuleType: "Diesel",
+
+    carInterior: {
+        audioSystem: true,
+        isLeather: false,
+        seats: 2,
+    },
+
+    modify: function(color){
+        this.color=color;
+    }
+};
+
+console.log("The car object:", car);
+console.log("The fuel type of Tarzann is: ", car.fuleType);
+
+var col="green";
+console.log("color is:", car.modify(col));
